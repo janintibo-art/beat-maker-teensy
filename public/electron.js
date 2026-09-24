632a2f98942b907eb59aaad33df55fc6bd3f8178
janintibo <janintibo@gmail.com>
2026-09-24 10:19:05 +0200

    v9: Fix electron entry point - use public/electron.js

--- public/electron.js
+++ public/electron.js
@@ -19,7 +19,6 @@ function createWindow() {
     : `file://${path.join(__dirname, '../build/index.html')}`;
 
   mainWindow.loadURL(startUrl);
-  
   if (isDev) {
     mainWindow.webContents.openDevTools();
   }
@@ -30,13 +29,11 @@ function createWindow() {
 }
 
 app.on('ready', createWindow);
-
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
-
 app.on('activate', () => {
   if (mainWindow === null) {
     createWindow();
